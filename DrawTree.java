0f19083c4b073eecfb815519ebe405eaf6181890
IsaWdx <isachriswang@163.com>
2016-12-02 13:32:07 +0800

    Draw tree with test expression

---
+++ DrawTree.java
@@ -0,0 +1,28 @@
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.util.Arrays;
+
+public class DrawTree {
+    public static void main(String[] args) throws Exception {
+        FileInputStream text = new FileInputStream("D:\\Study\\compiler\\src\\Factorial.txt");
+        ANTLRInputStream input = new ANTLRInputStream(text);
+        MiniJavaLexer lexer = new MiniJavaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        MiniJavaParser parser = new MiniJavaParser(tokens);
+        ParseTree tree = parser.goal();
+        JFrame frame = new JFrame("Antlr AST");
+        JPanel panel = new JPanel();
+        TreeViewer viewer = new TreeViewer(Arrays.asList(parser.getRuleNames()), tree);
+        viewer.setScale(1);
+        panel.add(viewer);
+        JScrollPane scrPane = new JScrollPane(panel);
+        frame.add(scrPane);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(1500,500);
+        frame.setVisible(true);
+    }
+}
